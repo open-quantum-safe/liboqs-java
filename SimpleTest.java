2994d51e21c0686daa0d09751aa30239777adfa7
Dimitris Mouris <jimouris@udel.edu>
2020-03-17 18:30:31 -0400

    Add is_KEM_supported, get_supported_KEMs and get_enabled_KEMs

--- SimpleTest.java
+++ SimpleTest.java
@@ -1,6 +1,15 @@
 import oqs.KEMs;
+import java.util.ArrayList;
 
 public class SimpleTest {
+    
+    public static <E, T extends Iterable<E>> void print_list(T list){
+        for (Object element : list){
+            System.out.print(element);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
     
     public static void main(String[] args) {
         // test max_number_KEMs
@@ -19,6 +28,23 @@ public class SimpleTest {
         System.out.println("get_KEM_name(" + alg_id + "): " + KEMs.get_KEM_name(alg_id));
         alg_id = 5;
         System.out.println("get_KEM_name(" + alg_id + "): " + KEMs.get_KEM_name(alg_id));
+        System.out.println();
+        
+        // test is_KEM_supported
+        alg = "foobar";
+        System.out.println("is_KEM_supported(\"" + alg + "\"): " + KEMs.is_KEM_supported(alg));
+        alg = "Kyber768";
+        System.out.println("is_KEM_supported(\"" + alg + "\"): " + KEMs.is_KEM_supported(alg));
+        System.out.println();
+        
+        // test get_supported_KEMs
+        ArrayList<String> supported_KEMs = KEMs.get_supported_KEMs();
+        print_list(supported_KEMs);
+        System.out.println();
+        
+        // test get_enabled_KEMs
+        ArrayList<String> enabled_KEMs = KEMs.get_enabled_KEMs();
+        print_list(enabled_KEMs);
         System.out.println();
         
     }
--- oqs/KEMs.java
+++ oqs/KEMs.java
@@ -1,5 +1,7 @@
 package oqs;
 
+import java.util.ArrayList;
+
 /**
  * \class oqs::KEMs
  * \brief Singleton class, contains details about supported/enabled key exchange
@@ -35,4 +37,41 @@ public class KEMs {
      */
     public static native String get_KEM_name(long alg_id);
 
+
+    /**
+     * \brief ArrayList of supported KEM algorithms
+     * \return ArrayList of supported KEM algorithms
+     */
+    public static ArrayList<String> get_supported_KEMs() {
+        ArrayList<String> supported_KEMs = new ArrayList<>();
+        for (int i = 0; i < max_number_KEMs(); ++i) {
+            supported_KEMs.add(get_KEM_name(i));
+        }
+        return supported_KEMs;
+    }
+    
+    /**
+     * \brief Vector of enabled KEM algorithms
+     * \return Vector of enabled KEM algorithms
+     */
+    public static ArrayList<String> get_enabled_KEMs() {
+        ArrayList<String> enabled_KEMs = new ArrayList<>();
+        for (String elem : get_supported_KEMs()) {
+            if (is_KEM_enabled(elem)) {
+                enabled_KEMs.add(elem);
+            }
+        }
+        return enabled_KEMs;
+    }
+    
+    /**
+     * \brief Checks whether the KEM algorithm \a alg_name is supported
+     * \param alg_name Cryptographic algorithm name
+     * \return True if the KEM algorithm is supported, false otherwise
+     */
+    public static boolean is_KEM_supported(String alg_name) {
+        ArrayList<String> supported_KEMs = get_supported_KEMs();
+        return supported_KEMs.contains(alg_name);
+    }
+    
 }
