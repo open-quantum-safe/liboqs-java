3b4b3ecacdb92840ca9cef9b5b07e239e74b73c9
Dimitris Mouris <jimouris@udel.edu>
2020-03-22 11:38:10 -0400

    Add Common class for commonly used static methods

---
+++ src/main/java/org/openquantumsafe/Common.java
@@ -0,0 +1,35 @@
+package org.openquantumsafe;
+
+public class Common {
+    
+    public static <E, T extends Iterable<E>> void print_list(T list) {
+        for (Object element : list){
+            System.out.print(element);
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    public static String chopHex(byte[] bytes) {
+        final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int num = 8;
+        for (int i = 0; i < num; i++) {
+            int v = bytes[i] & 0xFF;
+            sb.append(HEX_ARRAY[v >>> 4]);
+            sb.append(HEX_ARRAY[v & 0x0F]);
+            sb.append(" ");
+        }
+        if (bytes.length > num*2) {
+            sb.append("... ");
+        }
+        for (int i = bytes.length - num; i < bytes.length; i++) {
+            int v = bytes[i] & 0xFF;
+            sb.append(HEX_ARRAY[v >>> 4]);
+            sb.append(HEX_ARRAY[v & 0x0F]);
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+}
--- src/test/java/org/openquantumsafe/KEM_Test.java
+++ src/test/java/org/openquantumsafe/KEM_Test.java
@@ -5,44 +5,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class KEM_Test {
-    
-    public static <E, T extends Iterable<E>> void print_list(T list){
-        for (Object element : list){
-            System.out.print(element);
-            System.out.print(" ");
-        }
-        System.out.println();
-    }
-    
-    public static String shortHex(byte[] bytes) {
-        final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-        StringBuilder sb = new StringBuilder();
-        int num = 8;
-        for (int i = 0; i < num; i++) {
-            int v = bytes[i] & 0xFF;
-            sb.append(HEX_ARRAY[v >>> 4]);
-            sb.append(HEX_ARRAY[v & 0x0F]);
-            sb.append(" ");
-        }
-        if (bytes.length > num*2) {
-            sb.append("... ");
-        }
-        for (int i = bytes.length - num; i < bytes.length; i++) {
-            int v = bytes[i] & 0xFF;
-            sb.append(HEX_ARRAY[v >>> 4]);
-            sb.append(HEX_ARRAY[v & 0x0F]);
-            sb.append(" ");
-        }
-        return sb.toString();
-    }
-    
+
     public static void main(String[] args) {
         System.out.println("Supported KEMs:");
-        print_list(KEMs.get_supported_KEMs());
+        Common.print_list(KEMs.get_supported_KEMs());
         System.out.println();
         
         System.out.println("Enabled KEMs:");
-        print_list(KEMs.get_enabled_KEMs());
+        Common.print_list(KEMs.get_enabled_KEMs());
         System.out.println();
         
         String kem_name = "DEFAULT";
@@ -58,7 +28,7 @@ public class KEM_Test {
         byte[] client_sk = client.export_secret_key();
         
         System.out.println("Client public key:");
-        System.out.println(shortHex(client_public_key));
+        System.out.println(Common.chopHex(client_public_key));
         System.out.println("\nIt took " + timeElapsed + " millisecs to generate the key pair.");
         
         KeyEncapsulation server = new KeyEncapsulation(kem_name);
@@ -74,9 +44,9 @@ public class KEM_Test {
         System.out.println("It took " + (System.currentTimeMillis() - t) + " millisecs to decapsulate the secret.");
 
         System.out.println("\nClient shared secret:");
-        System.out.println(shortHex(shared_secret_client));
+        System.out.println(Common.chopHex(shared_secret_client));
         System.out.println("\nServer shared secret:");
-        System.out.println(shortHex(shared_secret_server));
+        System.out.println(Common.chopHex(shared_secret_server));
         
         System.out.println("\nShared secrets coincide? " + Arrays.equals(shared_secret_client, shared_secret_server));
     }
