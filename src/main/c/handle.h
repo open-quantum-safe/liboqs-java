dbb8f5c4a79289e0e800bcacc47578b921f8ca4a
Dimitris Mouris <jimouris@udel.edu>
2020-03-22 13:57:41 -0400

    Split handle into source and header file

--- src/main/c/handle.h
+++ src/main/c/handle.h
@@ -1,22 +1,13 @@
 #ifndef _HANDLE_H_INCLUDED_
 #define _HANDLE_H_INCLUDED_
 
+#include <jni.h>
 #include <oqs/oqs.h>
 
-jfieldID getHandleField(JNIEnv *env, jobject obj, char* handle_field) {
-    jclass c = (*env)->GetObjectClass(env, obj);
-    return (*env)->GetFieldID(env, c, handle_field, "J");
-}
+jfieldID getHandleField(JNIEnv *, jobject, char *);
 
-// use long to store pointer
-void *getHandle(JNIEnv *env, jobject obj, char* handle_field) {
-    jlong handle = (*env)->GetLongField(env, obj, getHandleField(env, obj, handle_field));
-    return (void *)(handle);
-}
+void *getHandle(JNIEnv *, jobject, char *);
 
-void setHandle(JNIEnv *env, jobject obj, void *t, char* handle_field) {
-    jlong handle = (jlong) (t);
-    (*env)->SetLongField(env, obj, getHandleField(env, obj, handle_field), handle);
-}
+void setHandle(JNIEnv *, jobject, void *, char *);
 
 #endif
