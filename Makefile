d2b64205037bb292cad15ff65c45d5c7e872a0bb
Dimitris Mouris <jimouris@udel.edu>
2020-03-23 02:05:52 -0400

    Add wrappers for liboqs rand functions

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
-all: liboqs-jni KEM_example Sig_example
+all: liboqs-jni KEM_example Sig_example Rand_example
 
 KEM_example: liboqs-jni
 	javac -classpath src/main/java/ src/test/java/org/openquantumsafe/KEM_Test.java
 
 Sig_example: liboqs-jni
 	javac -classpath src/main/java/ src/test/java/org/openquantumsafe/Sig_Test.java
+
+Rand_example: liboqs-jni
+	javac -classpath src/main/java/ src/test/java/org/openquantumsafe/Rand_Test.java
 
 liboqs-jni:
 	$(MAKE) -C src/main/c
@@ -14,6 +17,9 @@ run-kem:
 
 run-sig:
 	java -classpath src/main/java/:src/test/java/ -Djava.library.path=./src/main/c/ org.openquantumsafe.Sig_Test
+
+run-rand:
+	java -classpath src/main/java/:src/test/java/ -Djava.library.path=./src/main/c/ org.openquantumsafe.Rand_Test
 
 clean-c:
 	$(MAKE) -C src/main/c clean
---
+++ src/test/java/org/openquantumsafe/Rand_Test.java
@@ -0,0 +1,26 @@
+package org.openquantumsafe;
+
+import org.openquantumsafe.*;
+
+public class Rand_Test {
+
+    public static void main(String[] args) {
+        Rand.randombytes_switch_algorithm("NIST-KAT");
+        byte[] entropy_seed = new byte[48];
+        entropy_seed[0] = (byte) 100;
+        entropy_seed[20] = (byte) 200;
+        entropy_seed[47] = (byte) 150;
+        Rand.randombytes_nist_kat_init(entropy_seed);
+        System.out.println("NIST-KAT:\t\t" + Common.to_hex(Rand.randombytes(32)));
+
+        String os = System.getProperty("os.name");
+        if (!os.equals("Windows")) {
+            Rand.randombytes_switch_algorithm("OpenSSL");
+            System.out.println("OpenSSL:\t\t" + Common.to_hex(Rand.randombytes(32)));
+        }
+
+        Rand.randombytes_switch_algorithm("system");
+        System.out.println("System (default):\t" + Common.to_hex(Rand.randombytes(32)));
+    }
+
+}
