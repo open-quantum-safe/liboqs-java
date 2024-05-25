945035342c691bbef0bcd3df945dd185b666846c
xybei <xybei@outlook.com>
2024-05-25 14:47:43 +0800

    Fix warning: Raw use of parameterized class 'Pair'
    Signed-off-by: xybei <xybei@outlook.com>

--- src/main/java/org/openquantumsafe/Pair.java
+++ src/main/java/org/openquantumsafe/Pair.java
@@ -21,8 +21,8 @@ public class Pair<L, R> {
 
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Pair)) return false;
-        Pair pairo = (Pair) o;
+        if (!(o instanceof Pair<?, ?>)) return false;
+        Pair<?, ?> pairo = (Pair<?, ?>) o;
         return this.left.equals(pairo.getLeft()) && this.right.equals(pairo.getRight());
     }
 
