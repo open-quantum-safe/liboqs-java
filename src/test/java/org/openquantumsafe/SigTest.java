75fcbcfea33adc87b4e4e49e41e37c427b5d9acd
John Gray <john.gray@entrust.com>
2025-01-13 13:39:30 -0500

    Add support for the ML-DSA Context
    Signed-off-by: John Gray <john.gray@entrust.com>

--- src/test/java/org/openquantumsafe/SigTest.java
+++ src/test/java/org/openquantumsafe/SigTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+
+// import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class SigTest {
@@ -22,6 +25,7 @@ public class SigTest {
     public static void init(){
         System.out.println("Initialize list of enabled Signatures");
         enabled_sigs = Sigs.get_enabled_sigs();
+        System.out.println("Enabled signatures: [" + enabled_sigs + "]" );
     }
 
     /**
@@ -53,6 +57,36 @@ public class SigTest {
         sb.append("\033[0;32m").append("PASSED").append("\033[0m");
         System.out.println(sb.toString());
     }
+    
+    /**
+     * Test Sigs with context.
+     */
+    @ParameterizedTest(name = "Testing {arguments}")
+    @MethodSource("getContextSupportedAlgsAsStream")
+    public void testSigsWithContext(String sig_name) {
+    	byte[] context = "01234567890".getBytes();
+        StringBuilder sb = new StringBuilder();
+        sb.append(sig_name);
+        sb.append(String.format("%1$" + (40 - sig_name.length()) + "s", ""));
+
+        // Create signer and verifier
+        Signature signer = new Signature(sig_name);
+        Signature verifier = new Signature(sig_name);
+        
+         // Generate signer key pair
+        byte[] signer_public_key = signer.generate_keypair();
+
+        // Sign the message
+        byte[] signature = signer.sign(message, context);
+
+        // Verify the signature
+        boolean is_valid = verifier.verify(message, signature, context, signer_public_key);
+        assertTrue(is_valid, sig_name);
+        
+        // If successful print Sig name, otherwise an exception will be thrown
+        sb.append("\033[0;32m").append("PASSED").append("\033[0m");
+        System.out.println(sb.toString());
+    }
 
     /**
      * Test the MechanismNotSupported Exception
@@ -69,4 +103,12 @@ public class SigTest {
         return enabled_sigs.parallelStream();
     }
 
+    /**
+     * Method to convert the list of ML-DSA Sigs to a stream for input to testAllSigs
+     */
+    private static Stream<String> getContextSupportedAlgsAsStream() {
+    	return Arrays.asList(
+                "ML-DSA-44", "ML-DSA-65", "ML-DSA-87"
+            ).parallelStream();
+    }
 }
